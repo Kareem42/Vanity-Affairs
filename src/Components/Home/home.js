8f994d0e458b02b6126b42e51c40e5e3685a43cd
Justin Alcendor <kareem4209@gmail.com>
2021-10-23 18:21:34 -0500

    changed color and set up pages

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -2,11 +2,9 @@ import React from "react";
 import './App.css';
 
 const Home = () => {
-    <div>
-        <h1>If you are looking for advice for your next business idea, look no further</h1>
-        <p>We will provide you with the best websites to get your business up and running right away.</p>
-    </div>
+    <>
 
+    </>
 }
 
 export default Home;
